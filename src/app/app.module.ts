6f47b011732ae9278dd6214489e738504a15cd59
Richard <richard.tarassov@hotmail.com>
2025-01-16 19:40:46 +0200

    Removed unnecessary components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { ClickableEggComponent } from './components/clickable-egg/clickable-egg.component';
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
@@ -15,7 +14,6 @@ import {HttpClientModule} from "@angular/common/http";
     AppComponent,
     HeaderComponent,
     HomePageComponent,
-    FooterComponent,
     ClickableEggComponent
   ],
   imports: [
